c4b57a4e635b78b0092d168564cbd1141fea8748
Noopur Nishikant Zambare <noopurnishikantzambare@Noopurs-Air.hitronhub.home>
2023-07-05 17:12:08 -0600

    up

--- src/data/InternshipsData.js
+++ src/data/InternshipsData.js
@@ -8,7 +8,7 @@ export const InternshipsList = [
       tech_stack: ["AWS", "Machine Learning","Express JS", "Node JS", "React", ],
     },
     {
-      img: "./public/amazon_ml.png",
+      img: "./amazon_ml.png",
       title: "Amazon ML Summer School",
       duration: "Apprenticeship | July 2022",
       description: "Competed with the engineering students in India in programming skills, logical reasoning, mathematics and machine learning assessments and was selected among the top few students for this training session.",
@@ -16,14 +16,14 @@ export const InternshipsList = [
       certificate : "https://drive.google.com/file/d/1Y9PfVw-3b-3RC9krOv3-0uxYdggWjdy5/view?usp=sharing",
     },
     {
-      img: "./public/automation_edge.png",
+      img: "./automation_edge.png",
       title: "Machine Learning Intern",
       duration: "Remote | June 2022 - July 2022",
       description: "Performed text analysis and intent recognition using Named Entity Recognition and Part Of Speech tagging." ,
       tech_stack: ['Machine Learning', 'Deep learning', 'NLP', 'Spacy', 'Textacy'],
     },
     {
-      img: "./public/automation_edge.png",
+      img: "./automation_edge.png",
       title: "Data Science Intern",
       duration: "Remote | Dec 2021 - Jan 2022",
       description: "Performed IT Ticket Discovery and Exploratory Data Analysis to identify automation opportunities and designed ChatBots for HR using BOT Framework Composer",

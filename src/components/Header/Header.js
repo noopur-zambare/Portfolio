6f61716ef9da63616fb4c94d9323efb53a584102
Noopur Nishikant Zambare <noopurnishikantzambare@Noopurs-Air.hitronhub.home>
2023-07-24 09:41:42 -0600

    up

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Nav, NavLink, Bars, NavMenu } from "./HeaderElements";
-import './Header.css';
-
+import "./Header.css";
+import PDFViewer from './PDFviewer'
 const Header = ({ toggle }) => {
-  return (
+  const [showPDF, setShowPDF] = useState(false);
 
+  const handleShowPDF = () => {
+    setShowPDF(true);
+  };
+
+  const handleClosePDF = () => {
+    setShowPDF(false);
+  };
+
+  return (
+    <>
       <Nav className="Nav">
         <NavMenu>
           <NavLink className="menu-item" to="about">
@@ -21,9 +31,13 @@ const Header = ({ toggle }) => {
           </NavLink>
           
         </NavMenu>
+        <a href="https://drive.google.com/file/d/1BvRIRZbD6MqOgCJsinZQjLA8akoFnikN/view?usp=drive_link" target="_blank" rel="noopener noreferrer" style={{marginRight:'100px'}}>
+            Resume
+          </a>
         <Bars onClick={toggle} />
       </Nav>
-
+   
+    </>
   );
 };
 
